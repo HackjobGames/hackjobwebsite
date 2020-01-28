dd5128fa1c68b5f7a6bec9102c22810248b5a233
DylanKersting <kerst09@umn.edu>
2020-01-27 21:43:56 -0600

    proxy doesnt matter

--- src/js/server_requests.js
+++ src/js/server_requests.js
@@ -3,10 +3,6 @@ import axios from 'axios/dist/axios'
 const getDevlogs = () => {
     return axios('http://localhost:3000',
     {
-	    proxy: {
-	        host: '127.0.0.1',
-            port: 4400,
-	    },
         headers: {
             request: 'devlogMain'
         }
