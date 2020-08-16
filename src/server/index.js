9c15679119d5ed53d816136367d85d9d6136e5b2
Dylan Kersting <kerst098@umn.edu>
2020-08-15 20:58:40 -0500

    add ssl

--- src/server/index.js
+++ src/server/index.js
@@ -2,11 +2,18 @@ import Koa from 'koa'
 import query from './query'
 import serve from 'koa-static'
 import path from 'path'
+import ssl from 'koa-sslify'
+import https from 'https'
+import fs from 'fs'
 const app = new Koa()
 
 const requests = {
     devlogMain: () => {return query('SELECT * FROM devlog')}
 }
+
+app.use(ssl({
+  port: 4400
+}))
 
 app.use(serve(path.resolve('dist')))
 
@@ -14,6 +21,11 @@ app.use(serve(path.resolve('dist')))
 app.use(async (ctx) => {
   console.log(ctx)
 })
+
+const options = {
+  key: fs.readFileSync('private.key'),
+  cert: fs.readFileSync('certificate.crt')
+}
 
 app.listen(3000)
 console.log('Listening...')
