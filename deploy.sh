a888e866af833227aa1f8a3cd3f5ddf81d669f31
DylanKersting <dylankersting@gmail.com>
2020-11-05 00:42:29 -0600

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
-sudo kill $(sudo netstat -anp | awk '/ LISTEN / {if($4 ~ ":80$") { gsub("/.*","",$7); print $7; exit } }')
-sudo kill $(sudo netstat -anp | awk '/ LISTEN / {if($4 ~ ":443$") { gsub("/.*","",$7); print $7; exit } }')
 git fetch origin
 git reset --hard origin/master
 npm install
 npm run build
-sudo npm run serve
+sudo systemctl daemon-reload
+sudo systemctl restart hackjobgames.service
