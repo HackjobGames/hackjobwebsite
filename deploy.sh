c11f3f32345b4223f605657cd24c019496495c98
DylanKersting <dylankersting@gmail.com>
2021-06-15 16:48:55 -0500

    remove build from deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 git fetch origin
 git reset --hard origin/master
 npm install
-npm run build
 npx prisma migrate dev --skip-generate
 sudo systemctl daemon-reload
 sudo systemctl restart hackjobgames.service
