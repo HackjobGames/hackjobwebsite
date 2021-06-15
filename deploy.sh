3e7a20c01a7d12611bdd539407c29994bb4c5592
DylanKersting <dylankersting@gmail.com>
2021-06-15 15:57:13 -0500

    deploy doesnt work

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ git fetch origin
 git reset --hard origin/master
 npm install
 npm run build
-npx prisma migrate deploy
+npx prisma migrate dev --skip-generate
 sudo systemctl daemon-reload
 sudo systemctl restart hackjobgames.service
 sudo journalctl -f -u hackjobgames
